591eaa05af2ee5366bd451e497b6ec1dc548dd66
Denis Olshin <me@denull.ru>
2019-03-30 17:51:30 +0300

    basic a-text-input

---
+++ src/components/ATextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="a-text-input"
+    :class="classList">
+    <input
+      type="text"
+      class="a-text-input__input"
+      :value="value"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ATextInput',
+  props: {
+    value: String,
+    disabled: Boolean,
+  },
+  computed: {
+    classList() {
+      return [
+        this.disabled && `is-disabled`,
+      ];
+    }
+  },
+  methods: {
+  }
+}
+</script>
