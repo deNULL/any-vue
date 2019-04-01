48f0048ce8218ad6a8e84e88e88bbe19a2fff7c7
Denis Olshin <me@denull.ru>
2019-04-01 03:39:03 +0300

    added basis for action buttons behind list cells

--- src/components/AListItem.vue
+++ src/components/AListItem.vue
@@ -3,29 +3,43 @@
     class="a-list-item"
     :class="classList"
     @click="$emit('click')">
-    <slot name="image">
-      <div class="a-list-item__image" v-if="imageUrl" :style="{ background: `url(${imageUrl})` }"></div>
-    </slot>
-    <slot name="content">
-      <div class="a-list-item__content">
-        <div class="a-list-item__title" v-if="title"><slot>{{ title }}</slot></div>
-        <div class="a-list-item__label" v-if="label"><slot name="label">{{ label }}</slot></div>
+    <div class="a-list-item__actions is-left" v-if="actionsLeft && actionsLeft.length">
+      <div class="a-list-item__action" v-for="action in actionsLeft">
+        <div class="a-list-item__action-icon"></div>
+        {{ action.title }}
       </div>
-    </slot>
-    <slot name="value">
-      <div class="a-list-item__value" v-if="accessory !== 'switch' && value">{{ value }}</div>
-    </slot>
-    <slot name="accessory">
-      <a-switch
-        class="a-list-item__accessory"
-        v-if="accessory === 'switch'"
-        v-model="value"
-        :value-on="valueOn"
-        :value-off="valueOff"
-        @input="$emit('input', $event)"
-      ></a-switch>
-      <div class="a-list-item__accessory icon-chevron" v-if="accessory === 'chevron'"></div>
-    </slot>
+    </div>
+    <div class="a-list-item__actions is-right" v-if="actionsRight && actionsRight.length">
+      <div class="a-list-item__action" v-for="action in actionsRight">
+        <div class="a-list-item__action-icon"></div>
+        {{ action.title }}
+      </div>
+    </div>
+    <div class="a-list-item__body">
+      <slot name="image">
+        <div class="a-list-item__image" v-if="imageUrl" :style="{ background: `url(${imageUrl})` }"></div>
+      </slot>
+      <slot name="content">
+        <div class="a-list-item__content">
+          <div class="a-list-item__title" v-if="title"><slot>{{ title }}</slot></div>
+          <div class="a-list-item__label" v-if="label"><slot name="label">{{ label }}</slot></div>
+        </div>
+      </slot>
+      <slot name="value">
+        <div class="a-list-item__value" v-if="accessory !== 'switch' && value">{{ value }}</div>
+      </slot>
+      <slot name="accessory">
+        <a-switch
+          class="a-list-item__accessory"
+          v-if="accessory === 'switch'"
+          v-model="value"
+          :value-on="valueOn"
+          :value-off="valueOff"
+          @input="$emit('input', $event)"
+        ></a-switch>
+        <div class="a-list-item__accessory icon-chevron" v-if="accessory === 'chevron'"></div>
+      </slot>
+    </div>
   </div>
 </template>
 
@@ -57,6 +71,8 @@ export default {
     active: Boolean,
     selected: Boolean,
     disabled: Boolean,
+    actionsLeft: Array,
+    actionsRight: Array,
   },
   computed: {
     classList() {
